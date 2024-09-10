e2254e77e5ad6941354e179682eed6919a0356e7
max <max@macks.cloud>
2024-09-10 18:37:43 +0200

    nixvim colors and telescope

--- home/nixvim/default.nix
+++ home/nixvim/default.nix
@@ -6,5 +6,24 @@
 
   programs.nixvim = {
     enable = true;
+    plugins = {
+      telescope = {
+      	enable = true;
+      };
+    };
+
+
+    colorschemes.catppuccin.enable = true;
+    colorschemes.catppuccin.settings.transparent_background = true;
+
+    globals.mapleader = " ";
+
+    keymaps = [
+	{
+	  action = "<cmd>Telescope git_files<cr>";
+	  key = "<leader><leader>";
+	  options.silent = true;
+	}
+    ];
   };
 }
