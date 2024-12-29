4057156de9c9be862994471068e8e32975b0c3b9
max <max@macks.cloud>
2024-12-29 20:34:27 +0100

    add fish, eza

--- home/shell/default.nix
+++ home/shell/default.nix
@@ -1,9 +1,22 @@
 {
+
+  programs.fish = {
+    enable = true;
+    shellAliases = {
+      ls = "eza";
+      ll = "eza -lah";
+    };
+  };
   programs.zoxide = {
     enable = true;
     enableFishIntegration = true;
     options = [
       "--cmd cd"
     ];
+  };
+
+  programs.eza = {
+    enable = true;
+    enableFishIntegration = true;
   };
 }
