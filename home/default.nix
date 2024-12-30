a3e2e40685c8f17eb287462fb87aa307bf5e5848
max <max@macks.cloud>
2024-12-30 22:39:19 +0100

    add nix helper

--- home/default.nix
+++ home/default.nix
@@ -28,4 +28,11 @@
   programs.librewolf = {
     enable = true;
   };
+
+  programs.nh = {
+    enable = true;
+    clean.enable = true;
+    clean.extraArgs = "--keep-since 4d --keep 3";
+    flake = "/home/max/code/nixos-config";
+  };
 }
