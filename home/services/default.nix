668e488a19a7096b2db5824d2c2ad9c69e30c258
max <max@macks.cloud>
2025-01-11 17:22:58 +0100

    move services

---
+++ home/services/default.nix
@@ -0,0 +1,21 @@
+{ pkgs, ... }:
+{
+
+  home.packages = with pkgs; [
+    nextcloud-client
+  ];
+
+  services.gammastep = {
+    enable = true;
+    temperature.day = 5700;
+    temperature.night = 4000;
+  };
+
+  services.nextcloud-client = {
+    enable = true;
+    startInBackground = true;
+    package = pkgs.nextcloud-client;
+  };
+
+  services.ssh-agent.enable = true;
+}
