388ae02737f8babdbadd2bbfdca71fc832afc3c5
max <max@macks.cloud>
2025-01-11 14:37:41 +0100

    full update of river config
    to be up to date with my old dotfiles config

--- home/river/default.nix
+++ home/river/default.nix
@@ -1,4 +1,4 @@
-{ lib, config, ...}:
+{ lib, config, pkgs, ... }:
 
 with lib;
 let
@@ -21,63 +21,105 @@ in
   };
 
   config = mkIf cfg.enable {
-  wayland.windowManager.river = {
-    enable = true;
-    systemd.enable = false;
-    extraConfig = ''
-      rivertile -view-padding 2 -outer-padding 0 &
-    '';
-    settings = {
+    home.packages = [ pkgs.rivercarro ];
 
-      default-layout = "rivertile";
-      spawn = [
-        "yambar"
-      ];
+    wayland.windowManager.river = {
+      enable = true;
+      systemd.enable = true;
+      xwayland.enable = true;
+      extraConfig = ''
+        rivercarro -inner-gaps 2 -outer-gaps 2 -no-smart-gaps -per-tag &
+      '';
+      settings = {
 
-      keyboard-layout = if cfg.colemak 
-        then
-          "-options ctrl:swapcaps -variant colemak_dh us"
-        else
-          "eu";
+        default-layout = "rivercarro";
+        spawn = [
+          "yambar"
+        ];
 
-      input."pointer-2-7-*" = {
-        accel-profile = "adaptive";
-        natural-scroll = "enabled";
-        tap = "enabled";
-      };
+        keyboard-layout =
+          if cfg.colemak
+          then
+            "-options ctrl:swapcaps -variant colemak_dh us"
+          else
+            "eu";
 
-      set-repeat = "50 300";
+        input."pointer-2-7-*" = {
+          accel-profile = "adaptive";
+          natural-scroll = "enabled";
+          tap = "enabled";
+        };
 
-      background-color = "0x1e1e2e";
-      border-color-focused = "0xb4befe";
-      border-color-unfocused = "0x1e1e2ee6";
-      rule-add = "-app-id '*' ssd"; # server side decoration for every app
+        input."pointer-*".accel-profile = "flat";
+
+        set-repeat = "50 300";
+        set-cursor-warp = "on-output-change";
+
+        background-color = "0x1e1e2e";
+        border-color-focused = "0xb4befe";
+        border-color-unfocused = "0x1e1e2ee6";
+        rule-add = "-app-id '*' ssd"; # server side decoration for every app
 
 
-      map = {
-        normal = builtins.listToAttrs
-          (
-            [
-              { name = "Super Return"; value = "spawn foot"; }
-              { name = "Super+Shift Return"; value = "spawn librewolf"; }
-              { name = "Super P"; value = "spawn 'fuzzel -I'"; }
+        map = {
+          normal = builtins.listToAttrs
+            (
+              [
+                { name = "Super Return"; value = "spawn foot"; }
+                { name = "Super+Shift Return"; value = "spawn librewolf"; }
+                { name = "Super P"; value = "spawn 'fuzzel -I'"; }
+                { name = "Super E"; value = "spawn thunar"; }
 
-              { name = "Super+Shift C"; value = "close"; }
-              { name = "Super+Shift Q"; value = "exit"; }
+                { name = "Super+Shift C"; value = "close"; }
+                { name = "Super+Shift Q"; value = "exit"; }
 
-              { name = "Super Down"; value = "focus-view next"; }
-              { name = "Super Up"; value = "focus-view previous"; }
-              { name = "Super+Shift Down"; value = "swap next"; }
-              { name = "Super+Shift Up"; value = "swap previous"; }
-            ] ++
-            # Other tag-related keybindings (toggle-focused, set-view, etc.)
-            (keyBindingsForTags { keyPrefix = "Super"; command = "set-focused-tags"; }) ++
-            (keyBindingsForTags { keyPrefix = "Super+Control"; command = "toggle-focused-tags"; }) ++
-            (keyBindingsForTags { keyPrefix = "Super+Shift"; command = "set-view-tags"; }) ++
-            (keyBindingsForTags { keyPrefix = "Super+Shift+Control"; command = "toggle-view-tags"; })
-          );
+                { name = "Super Down"; value = "focus-view next"; }
+                { name = "Super Up"; value = "focus-view previous"; }
+                { name = "Super+Shift Down"; value = "swap next"; }
+                { name = "Super+Shift Up"; value = "swap previous"; }
+
+                { name = "Super Period"; value = "focus-output next"; }
+                { name = "Super Comma"; value = "focus-output previous"; }
+                { name = "Super Period"; value = "focus-output next"; }
+                { name = "Super Comma"; value = "focus-output previous"; }
+
+                { name = "Super+Shift Period"; value = "send-to-output -current-tags next"; }
+                { name = "Super+Shift Comma"; value = "send-to-output -current-tags previous"; }
+                {
+                  name = "Super O";
+                  value = "spawn 'sh -c \"riverctl send-to-output -current-tags next && riverctl focus-output next\"'";
+                }
+
+                { name = "Super M"; value = "zoom"; }
+                { name = "Super Left"; value = "send-layout-cmd rivercarro 'main-ratio -0.05'"; }
+                { name = "Super Right"; value = "send-layout-cmd rivercarro 'main-ratio +0.05'"; }
+                { name = "Super+Shift Left"; value = "send-layout-cmd rivercarro 'main-count +1'"; }
+                { name = "Super+Shift Right"; value = "send-layout-cmd rivercarro 'main-ratio -1'"; }
+
+                { name = "Super Space"; value = "send-layout-cmd rivercarro 'main-location-cycle left,monocle'"; }
+                { name = "Super F"; value = "toggle-fullscreen"; }
+              ] ++
+              # Other tag-related keybindings (toggle-focused, set-view, etc.)
+              (keyBindingsForTags {
+                keyPrefix = "Super";
+                command = "set-focused-tags";
+              }) ++
+              (keyBindingsForTags {
+                keyPrefix = "Super+Control";
+                command = "toggle-focused-tags";
+              }) ++
+              (keyBindingsForTags {
+                keyPrefix = "Super+Shift";
+                command = "set-view-tags";
+              }) ++
+              (keyBindingsForTags {
+                keyPrefix = "Super+Shift+Control";
+                command = "toggle-view-tags";
+              })
+            );
+        };
       };
     };
   };
-};
 }
+
