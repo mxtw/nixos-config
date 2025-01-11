9db66a5bf1acffb48cce39d67e118eaa8b341bb4
max <max@macks.cloud>
2025-01-12 00:02:44 +0100

    set up git commit signing

--- home/git/default.nix
+++ home/git/default.nix
@@ -4,6 +4,13 @@
 
     userEmail = "max@macks.cloud";
     userName = "max";
+
+    signing.key = "/home/max/.ssh/id_ed25519.pub";
+    signing.signByDefault = true;
+
+    extraConfig = {
+      gpg.format = "ssh";
+    };
   };
 
   programs.lazygit.enable = true;
