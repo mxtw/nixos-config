fc26835e6bc90c87f515a727e650eecc3c7317fa
max <max@macks.cloud>
2025-01-12 11:14:22 +0100

    cursor settings

--- home/default.nix
+++ home/default.nix
@@ -32,6 +32,9 @@
     theme.package = pkgs.magnetic-catppuccin-gtk;
     iconTheme.name = "Papirus";
     iconTheme.package = pkgs.papirus-icon-theme;
+    cursorTheme.name = "catppuccin-mocha-dark-cursors";
+    cursorTheme.package = pkgs.catppuccin-cursors.mochaDark;
+    cursorTheme.size = 16;
   };
 
   programs.nh = {
