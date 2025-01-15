2a9273f457e0aa6d9e28c77ef5b5583eaa40a029
max <max@macks.cloud>
2025-01-15 13:58:54 +0100

    add a few common dev tools
    cba setting up flakes for every throwaway tool i deal with

---
+++ home/dev/default.nix
@@ -0,0 +1,16 @@
+{ pkgs, ... }:
+{
+  # define a bunch of language tooling here because i cba setting up flakes for every throwaway thing
+  home.packages = with pkgs; [
+    gcc
+    gnumake
+
+    python3
+    uv
+
+    cargo
+    rustc
+
+    go
+  ];
+}
