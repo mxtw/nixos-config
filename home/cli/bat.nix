db4455bdac4e81a94ac2a0626e6f42787727eb80
max <max@macks.cloud>
2025-01-16 10:57:40 +0100

    enable bat

---
+++ home/cli/bat.nix
@@ -0,0 +1,18 @@
+{ pkgs, ... }:
+{
+  programs.bat = {
+    enable = true;
+    themes = {
+      catppuccin = {
+        src = pkgs.fetchFromGitHub {
+          owner = "catppuccin";
+          repo = "bat";
+          rev = "699f60fc8ec434574ca7451b444b880430319941";
+          hash = "sha256-6fWoCH90IGumAMc4buLRWL0N61op+AuMNN9CAR9/OdI=";
+        };
+        file = "themes/Catppuccin Mocha.tmTheme";
+      };
+    };
+    config.theme = "catppuccin";
+  };
+}
