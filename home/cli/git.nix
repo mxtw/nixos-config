02e58f2eaa6ea2e1eb7052eddd1f43e1e842af2f
max <max@macks.cloud>
2025-01-16 11:21:26 +0100

    enable difftastic

--- home/cli/git.nix
+++ home/cli/git.nix
@@ -11,21 +11,27 @@
     extraConfig = {
       gpg.format = "ssh";
     };
+
+    difftastic.enable = true;
   };
 
   programs.lazygit = {
     enable = true;
-    settings.gui.theme = {
-      activeBorderColor = [ "#b4befe" "bold" ];
-      inactiveBorderColor = [ "#a6adc8" ];
-      optionsTextColor = [ "#89b4fa" ];
-      selectedLineBgColor = [ "#313244" ];
-      cherryPickedCommitBgColor = [ "#45475a" ];
-      cherryPickedCommitFgColor = [ "#b4befe" ];
-      unstagedChangesColor = [ "#f38ba8" ];
-      defaultFgColor = [ "#cdd6f4" ];
-      searchingActiveBorderColor = [ "#f9e2af" ];
-      authorColors."*" = [ "#b4befe" ];
+    settings = {
+      git.paging.externalDiffCommand = "difft --color=always";
+
+      gui.theme = {
+        activeBorderColor = [ "#b4befe" "bold" ];
+        inactiveBorderColor = [ "#a6adc8" ];
+        optionsTextColor = [ "#89b4fa" ];
+        selectedLineBgColor = [ "#313244" ];
+        cherryPickedCommitBgColor = [ "#45475a" ];
+        cherryPickedCommitFgColor = [ "#b4befe" ];
+        unstagedChangesColor = [ "#f38ba8" ];
+        defaultFgColor = [ "#cdd6f4" ];
+        searchingActiveBorderColor = [ "#f9e2af" ];
+        authorColors."*" = [ "#b4befe" ];
+      };
     };
   };
 }
