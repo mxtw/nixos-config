3d783a88451c4c9b71d7db672f23c0fc018b7380
max <max@macks.cloud>
2025-01-19 14:14:36 +0100

    obs vaapi

--- home/media/video.nix
+++ home/media/video.nix
@@ -11,7 +11,13 @@
     };
   };
 
-  programs.obs-studio.enable = true;
+  programs.obs-studio = {
+    enable = true;
+    plugins = with pkgs.obs-studio-plugins; [
+      wlrobs
+      obs-vaapi
+    ];
+  };
 
   home.packages = with pkgs; [
     kdenlive
