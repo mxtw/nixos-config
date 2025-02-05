352407bc7c2e0ecb0de113334742ad5324458525
max <max@macks.cloud>
2025-02-05 16:41:23 +0100

    nixvim: replace cmp with blink

--- home/nixvim/plugins/lsp.nix
+++ home/nixvim/plugins/lsp.nix
@@ -32,67 +32,30 @@
 
     lsp-format.enable = true;
 
-    cmp = {
+    blink-cmp = {
       enable = true;
-      autoEnableSources = true;
-      settings.sources = [
-        { name = "nvim_lsp"; }
-        { name = "luasnip"; }
-        { name = "buffer"; }
-        { name = "path"; }
-      ];
-      settings.mapping = {
-        __raw = ''
-          cmp.mapping.preset.insert({
-            ['<CR>'] = cmp.mapping.confirm({
-                behavior = cmp.ConfirmBehavior.Replace,
-                select = false,
-            }),
-            ["<Up>"] = cmp.mapping.select_prev_item(),
-            ["<Down>"] = cmp.mapping.select_next_item(),
+      settings = {
+        sources.default = [ "lsp" "path" "snippets" "buffer" ];
+        signature.enabled = true;
+        completion.list.selection = {
+          auto_insert = true;
+          preselect = false;
+        };
 
-            ["<C-n>"] = cmp.mapping(function(fallback)
-                local has_words_before = function()
-                    unpack = unpack or table.unpack
-                    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-                    return col ~= 0 and
-                        vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-                end
-
-                if cmp.visible() then
-                    cmp.select_next_item()
-                    -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
-                    -- that way you will only jump inside the snippet region
-                elseif luasnip.expand_or_jumpable() then
-                    luasnip.expand_or_jump()
-                elseif has_words_before() then
-                    cmp.complete()
-                else
-                    fallback()
-                end
-            end, { "i", "s" }),
-
-            ["<C-p>"] = cmp.mapping(function(fallback)
-                if cmp.visible() then
-                    cmp.select_prev_item()
-                elseif luasnip.jumpable(-1) then
-                    luasnip.jump(-1)
-                else
-                    fallback()
-                end
-            end, { "i", "s" }),
-
-            ['<C-Space>'] = function(fallback)
-                if cmp.visible() then
-                    cmp.close()
-                else
-                    cmp.complete()
-                end
-            end,
-          })
-        '';
+        keymap = {
+          preset = "default";
+          "<C-Space>" = [ "show" "hide" "fallback" ];
+          "<CR>" = [ "accept" "fallback" ];
+          "<Down>" = [ "select_next" "fallback" ];
+          "<Up>" = [ "select_prev" "fallback" ];
+          "<C-n>" = [ "snippet_forward" "select_next" "fallback" ];
+          "<C-p>" = [ "snippet_backward" "select_prev" "fallback" ];
+          "<C-u>" = [ "scroll_documentation_up" "fallback" ];
+          "<C-d>" = [ "scroll_documentation_down" "fallback" ];
+        };
       };
     };
+
     luasnip.enable = true;
     friendly-snippets.enable = true;
   };
