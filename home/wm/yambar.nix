a7ff9154001583fbe86d0e70b5967954068c9d12
max <max@macks.cloud>
2025-02-08 11:59:00 +0100

    yambar: make right side modules togglable
    my desktop does not need to show me the battery %

--- home/wm/yambar.nix
+++ home/wm/yambar.nix
@@ -1,5 +1,8 @@
-{ config, ... }:
+{ lib, config, ... }:
+with lib;
 let
+  cfg = config.modules.yambar;
+
   tagDefault = {
     stack = [
       { background.color = "00000000"; }
@@ -26,122 +29,145 @@ let
   };
 in
 {
-  programs.yambar.enable = true;
-  programs.yambar.settings = {
+  options.modules.yambar = {
+    enable = mkEnableOption "yambar";
+    layout = mkEnableOption "yambar";
+    pulse = mkEnableOption "yambar";
+    cpu = mkEnableOption "yambar";
+    mem = mkEnableOption "yambar";
+    battery = mkEnableOption "yambar";
+    clock = mkEnableOption "yambar";
+  };
 
-    bar = {
-      location = "top";
-      font = "Hack";
-      height = 20;
-      right-spacing = 6;
-      right-margin = 6;
-      border = {
-        top-margin = 2;
-        left-margin = 2;
-        right-margin = 2;
-      };
-      background = "1e1e2ee6";
-      foreground = "b4befeff";
+  config = mkIf cfg.enable {
+    programs.yambar.enable = true;
+    programs.yambar.settings = {
 
-      center = [
-        {
-          river.title.string.text = "{title}";
-          river.title.string.max = 100;
-          river.content.map.conditions = { };
-        }
-      ];
+      bar = {
+        location = "top";
+        font = "Hack";
+        height = 20;
+        right-spacing = 6;
+        right-margin = 6;
+        border = {
+          top-margin = 2;
+          left-margin = 2;
+          right-margin = 2;
+        };
+        background = "1e1e2ee6";
+        foreground = "b4befeff";
 
-      left = [
-        {
-          river.anchors = [
-            { base = riverBase; }
-          ];
-          river.content.map = {
-            on-click = {
-              left = "sh -c \"riverctl set-focused-tags $((1 << ({id} - 1)))\"";
-              right = "sh -c \"riverctl toggle-focused-tags $((1 << ({id} - 1)))\"";
-              middle = "sh -c \"riverctl toggle-view-tags $((1 << ({id} - 1)))\"";
-            };
-            conditions = {
-              "state == urgent".map = riverBase // {
-                deco.background.color = "f9e2afff";
+        center = [
+          {
+            river.title.string.text = "{title}";
+            river.title.string.max = 100;
+            river.content.map.conditions = { };
+          }
+        ];
+
+        left = [
+          {
+            river.anchors = [
+              { base = riverBase; }
+            ];
+            river.content.map = {
+              on-click = {
+                left = "sh -c \"riverctl set-focused-tags $((1 << ({id} - 1)))\"";
+                right = "sh -c \"riverctl toggle-focused-tags $((1 << ({id} - 1)))\"";
+                middle = "sh -c \"riverctl toggle-view-tags $((1 << ({id} - 1)))\"";
               };
-              "state == focused".map = riverBase // {
-                deco = tagDefault;
-              };
-              "state == visible && ~occupied".map = riverBase;
-              "state == focused && occupied".map = riverBase // {
-                deco = tagDefault;
-              };
-              "state == unfocused".map = riverBase;
-              "state == invisible && ~occupied".empty = { };
-              "state == invisible && occupied".map = riverBase // {
-                deco.underline = {
-                  size = 3;
-                  color = "00000000";
+              conditions = {
+                "state == urgent".map = riverBase // {
+                  deco.background.color = "f9e2afff";
+                };
+                "state == focused".map = riverBase // {
+                  deco = tagDefault;
+                };
+                "state == visible && ~occupied".map = riverBase;
+                "state == focused && occupied".map = riverBase // {
+                  deco = tagDefault;
+                };
+                "state == unfocused".map = riverBase;
+                "state == invisible && ~occupied".empty = { };
+                "state == invisible && occupied".map = riverBase // {
+                  deco.underline = {
+                    size = 3;
+                    color = "00000000";
+                  };
                 };
               };
             };
-          };
-        }
-      ];
+          }
+        ];
 
-      right = [
-        {
-          river.title.string.text = "{layout}";
-          river.content.map.conditions = { };
-        }
-        {
-          pulse.content.map = {
-            conditions = {
-              "source_muted && sink_muted".string = {
-                text = "vol muted mic muted";
-                foreground = "f9e2afff";
+        right = [ ]
+
+          ++ (if cfg.layout then [
+          {
+            river.title.string.text = "{layout}";
+            river.content.map.conditions = { };
+          }
+        ] else [ ])
+
+          ++ (if cfg.pulse then [
+          {
+            pulse.content.map = {
+              conditions = {
+                "source_muted && sink_muted".string = {
+                  text = "vol muted mic muted";
+                  foreground = "f9e2afff";
+                };
+                source_muted.string = {
+                  text = "vol {sink_percent}% mic muted";
+                  foreground = "f9e2afff";
+                };
+                sink_muted.string = {
+                  text = "vol muted mic {source_percent}%";
+                  foreground = "f9e2afff";
+                };
+                online.string = {
+                  text = "vol {sink_percent}% mic {source_percent}%";
+                  foreground = "f9e2afff";
+                };
               };
-              source_muted.string = {
-                text = "vol {sink_percent}% mic muted";
-                foreground = "f9e2afff";
-              };
-              sink_muted.string = {
-                text = "vol muted mic {source_percent}%";
-                foreground = "f9e2afff";
-              };
-              online.string = {
-                text = "vol {sink_percent}% mic {source_percent}%";
-                foreground = "f9e2afff";
-              };
+              on-click.left = "pavucontrol";
             };
-            on-click.left = "pavucontrol";
-          };
-        }
-        {
-          cpu = {
-            poll-interval = 2500;
-            content.map.conditions."id < 0" = [
-              {
-                string.text = "cpu {cpu}%";
-                string.foreground = "a6e3a1ff";
-              }
-            ];
-          };
-        }
-        {
-          mem.poll-interval = 2500;
-          mem.content.string = {
-            text = "mem {used:gb}gb";
-            foreground = "89dcebff";
-          };
-        }
+          }
+        ] else [ ])
 
-        {
+          ++ (if cfg.cpu then [
+          {
+            cpu = {
+              poll-interval = 2500;
+              content.map.conditions."id < 0" = [
+                {
+                  string.text = "cpu {cpu}%";
+                  string.foreground = "a6e3a1ff";
+                }
+              ];
+            };
+          }
+        ] else [ ])
+
+          ++ (if cfg.mem then [
+          {
+            mem.poll-interval = 2500;
+            mem.content.string = {
+              text = "mem {used:gb}gb";
+              foreground = "89dcebff";
+            };
+          }
+        ] else [ ])
+
+          ++ (if cfg.battery then [{
           battery.name = "BAT0";
           battery.content.string = {
             text = "bat {capacity}% ({estimate} rem)";
             foreground = "74c7ecff";
           };
-        }
+        }] else [ ])
 
-        {
+          ++ (if cfg.clock then [{
           clock = {
             time-format = "%H:%M";
             date-format = "%Y-%m-%d";
@@ -150,27 +176,27 @@ in
               foreground = "f38ba8ff";
             };
           };
-        }
-      ];
+        }] else [ ]);
+      };
     };
-  };
 
-  home.file = {
-    "${config.xdg.configHome}/yambar/launch.sh" = {
-      text = ''
-        #!/bin/sh
-        killall yambar
+    home.file = {
+      "${config.xdg.configHome}/yambar/launch.sh" = {
+        text = ''
+          #!/bin/sh
+          killall yambar
 
-        monitors=$(wlr-randr | grep "^[^ ]" | awk '{ print$1 }')
-        total=$(wlr-randr | grep "^[^ ]" | awk '{ print$1 }' | wc -l)
+          monitors=$(wlr-randr | grep "^[^ ]" | awk '{ print$1 }')
+          total=$(wlr-randr | grep "^[^ ]" | awk '{ print$1 }' | wc -l)
 
-        for monitor in $monitors; do
-            riverctl focus-output $monitor
-            yambar &
-            sleep 0.2
-        done
-      '';
-      executable = true;
+          for monitor in $monitors; do
+              riverctl focus-output $monitor
+              yambar &
+              sleep 0.2
+          done
+        '';
+        executable = true;
+      };
     };
   };
 }
