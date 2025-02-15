44e09fbcca038aa90c1f11b0db6d8461adf0a97a
max <max@macks.cloud>
2025-02-15 11:46:25 +0100

    zipload: add zipload

--- home/cli/misc.nix
+++ home/cli/misc.nix
@@ -1,9 +1,22 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
+let
+  zipload = pkgs.buildGoModule {
+    src = pkgs.fetchFromGitHub {
+      owner = "mxtw";
+      repo = "zipload";
+      rev = "0fd75dcaeaee7ed971de6a52a53a19f7f11953d3";
+      sha256 = "jE09qpcq+TsbnAo/SwEMK8HiA2/uSYadyyQZ3Gv0gNQ=";
+    };
+    name = "zipload";
+    vendorHash = "sha256-3lgpC5jWgDpLi3zDXI+Kk+Qo1AC0pDY86BvBc4T3Kg0=";
+  };
+in
 {
   home.packages = with pkgs; [
     ncdu
     unzip
     jq
+    zipload
   ];
 
   programs.fastfetch.enable = true;
