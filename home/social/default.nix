a6c0d0237027e6b8acfbfd0f58e0c4a4db38dfe4
max <max@macks.cloud>
2025-02-18 18:25:06 +0100

    vesktop: older electron version to fix broken screen sharing

--- home/social/default.nix
+++ home/social/default.nix
@@ -1,7 +1,11 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [
-    vesktop
+    # use electron_33 to fix screen sharing bug until https://github.com/NixOS/nixpkgs/issues/380429 is fixed
+    (vesktop.override {
+      electron = electron_33;
+    })
+
     weechat
   ];
 }
