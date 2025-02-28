abb33c7fac2edcc2e6b2ab7cfd068b1c31811ce3
max <max@macks.cloud>
2025-02-28 16:43:01 +0100

    *: revert back to hack font
    i like iosevka but some glyphs seemed broken in terminal

--- home/wm/mako.nix
+++ home/wm/mako.nix
@@ -1,7 +1,7 @@
 {
   services.mako = {
     enable = true;
-    font = "Iosevka Nerd Font 10";
+    font = "Hack Nerd Font 10";
     textColor = "#b4befeff";
     borderColor = "#b4befeff";
     backgroundColor = "#1e1e2ee6";
