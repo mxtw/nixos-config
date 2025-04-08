7697f3127caeb84555dd90cbe2b2a3299c513d03
max <max@macks.cloud>
2025-04-08 17:45:02 +0200

    vesktop: remove electron override

--- home/social/default.nix
+++ home/social/default.nix
@@ -1,11 +1,7 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [
-    # use electron_33 to fix screen sharing bug until https://github.com/NixOS/nixpkgs/issues/380429 is fixed
-    (vesktop.override {
-      electron = electron_33;
-    })
-
+    vesktop
     weechat
 
     chatterino2
