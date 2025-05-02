9c3d612c2c7fde557ce6a0291d4b5493a936e7ef
max <max@macks.cloud>
2025-05-03 00:33:29 +0200

    laptop: enable zram swap

--- hosts/nixpad/hardware-configuration.nix
+++ hosts/nixpad/hardware-configuration.nix
@@ -5,7 +5,8 @@
 
 {
   imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
+    [
+      (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "nvme" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
@@ -14,17 +15,20 @@
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/3e87c6b4-5f9d-486b-b50e-08eac914d314";
+    {
+      device = "/dev/disk/by-uuid/3e87c6b4-5f9d-486b-b50e-08eac914d314";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/9F99-0B49";
+    {
+      device = "/dev/disk/by-uuid/9F99-0B49";
       fsType = "vfat";
       options = [ "fmask=0022" "dmask=0022" ];
     };
 
   swapDevices = [ ];
+  zramSwap.enable = true;
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
