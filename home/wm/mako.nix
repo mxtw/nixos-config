e008fb6c46ce8a674742f0ad9298703f1e0a210a
max <max@macks.cloud>
2025-05-04 20:47:42 +0200

    mako: rename deprecated config option

--- home/wm/mako.nix
+++ home/wm/mako.nix
@@ -1,9 +1,11 @@
 {
   services.mako = {
     enable = true;
-    font = "Hack Nerd Font 10";
-    textColor = "#b4befeff";
-    borderColor = "#b4befeff";
-    backgroundColor = "#1e1e2ee6";
+    settings = {
+      font = "Hack Nerd Font 10";
+      textColor = "#b4befeff";
+      borderColor = "#b4befeff";
+      backgroundColor = "#1e1e2ee6";
+    };
   };
 }
