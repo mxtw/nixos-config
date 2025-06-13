bdffa2550626d04238a34b1deb8bd422ff29824a
max <max@macks.cloud>
2025-06-13 23:08:23 +0200

    input: add vial, via for keyboard configuration

--- packages/input.nix
+++ packages/input.nix
@@ -1,7 +1,17 @@
+{ pkgs, ... }:
 {
   programs.weylus = {
     enable = true;
     users = [ "max" ];
     openFirewall = true;
   };
+
+  environment.systemPackages = with pkgs; [
+    vial
+    via
+  ];
+  services.udev.packages = with pkgs; [
+    vial
+    via
+  ];
 }
