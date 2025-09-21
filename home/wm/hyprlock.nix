29fb402d237c9e6b7c446e07d7115e8e1acc88f2
max <max@macks.cloud>
2025-09-21 14:02:54 +0200

    hyprlock: some improvements

--- home/wm/hyprlock.nix
+++ home/wm/hyprlock.nix
@@ -6,11 +6,13 @@
         {
           outline_thickness = 2;
           outer_color = "rgb(b4befe)";
+          check_color = "rgb(a6e3a1)";
+          fail_color = "rgb(f38ba8)";
           inner_color = "rgba(1e1e2e0a)";
           font_color = "rgb(cdd6f4)";
-          position = "0, 0";
+          position = "0, -120";
           fade_on_empty = false;
-          size = "200, 50";
+          size = "300, 40";
           rounding = -1;
         }
       ];
@@ -18,18 +20,19 @@
         color = "rgb(1e1e2e)";
         path = "screenshot";
         blur_passes = 4;
+        brightness = 0.5;
       };
       label = [
         {
           color = "rgb(cdd6f4)";
-          font_size = "70";
-          position = "0, 150";
-          text = "cmd[update:1000] date +%r";
+          font_size = "100";
+          position = "0, 50";
+          text = "cmd[update:1000] date +%H:%M:%S";
         }
         {
           color = "rgb(cdd6f4)";
-          font_size = "20";
-          position = "0, 80";
+          font_size = "32";
+          position = "0, -50";
           text = "cmd[update:10000] date +'%a, %x'";
         }
       ];
