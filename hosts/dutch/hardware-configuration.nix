ea11a12e62862ffa5a4bb4aa3470ec4624f61e46
max <max@macks.cloud>
2025-10-13 18:31:02 +0200

    dutch: noperm cifs mount
    this is not secure but whatever

--- hosts/dutch/hardware-configuration.nix
+++ hosts/dutch/hardware-configuration.nix
@@ -26,7 +26,7 @@
     fsType = "cifs";
     options =
       let
-        automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+        automount_opts = "noperm,x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
       in
       [ "${automount_opts},credentials=/run/secrets/smb-credentials" ];
   };
