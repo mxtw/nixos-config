652123088868d06fe609d8f49c006e2620aadcee
max <max@macks.cloud>
2025-10-14 16:18:46 +0200

    sdrpp: remove some not needed sources

--- home/media/radio.nix
+++ home/media/radio.nix
@@ -1,8 +1,22 @@
 { pkgs, ... }:
+let
+  sdrpp-slim = pkgs.sdrpp.override {
+    airspy_source = false;
+    airspyhf_source = false;
+    bladerf_source = false;
+    hackrf_source = false;
+    limesdr_source = false;
+    plutosdr_source = false;
+    rfspace_source = false;
+    soapy_source = false;
+    spyserver_source = false;
+  };
+in
 {
-  home.packages = with pkgs; [
-    sdrpp
-    chirp
-  ];
+  home.packages = with pkgs;
+    [
+      sdrpp-slim
+      chirp
+    ];
 }
 
