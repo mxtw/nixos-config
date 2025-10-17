b9a85c38df1f44d36a6beff7be42c349a49d406e
max <max@macks.cloud>
2025-10-17 20:17:32 +0200

    sops: split up in multiple sops files again

--- hosts/dutch/configuration.nix
+++ hosts/dutch/configuration.nix
@@ -22,7 +22,7 @@
     ];
   };
 
-  sops.defaultSopsFile = ../../secrets.yaml;
+  sops.defaultSopsFile = ./secrets.yaml;
   sops.age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
 
   environment.systemPackages = with pkgs;[
--- hosts/dutch/services/task.nix
+++ hosts/dutch/services/task.nix
@@ -1,11 +1,8 @@
-{ config, ... }:
 {
-  sops.secrets.taskwarrior_uuid = { };
   services.taskchampion-sync-server = {
     enable = true;
     host = "127.0.0.1";
     port = 10222;
-    allowClientIds = [ config.sops.secrets.taskwarrior_uuid.content ];
   };
   services.nginx.virtualHosts."task.macks.cloud" = {
     forceSSL = true;
