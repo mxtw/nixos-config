e65ae9a454d2dc9c1967c998ee0270b826d33833
max <max@macks.cloud>
2025-10-17 20:26:48 +0200

    taskwarrior: setup taskwarrior sync

--- home/cli/todo.nix
+++ home/cli/todo.nix
@@ -5,9 +5,18 @@
   programs.taskwarrior = {
     enable = true;
     package = pkgs.taskwarrior3;
+    config = {
+      sync.server.client_id = "125f47cf-db71-4da5-8371-70ea7f6c9768";
+      sync.server.url = "https://task.macks.cloud";
+    };
+    # extraconfig includes encryption secret
     extraConfig = ''
       include ${config.sops.secrets.taskwarrior-config.path}
     '';
+  };
+  services.taskwarrior-sync = {
+    enable = true;
+    package = pkgs.taskwarrior3;
   };
   programs.fish.shellAbbrs = {
     tw = "taskwarrior-tui";
--- hosts/dutch/services/task.nix
+++ hosts/dutch/services/task.nix
@@ -3,6 +3,7 @@
     enable = true;
     host = "127.0.0.1";
     port = 10222;
+    allowClientIds = [ "125f47cf-db71-4da5-8371-70ea7f6c9768" ];
   };
   services.nginx.virtualHosts."task.macks.cloud" = {
     forceSSL = true;
