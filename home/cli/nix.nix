f1ae55fd861269ef45c508e50f102126c413cb20
max <max@macks.cloud>
2025-10-24 22:42:56 +0200

    nix-search: improve search function

--- home/cli/nix.nix
+++ home/cli/nix.nix
@@ -18,10 +18,21 @@
       };
     };
     fish.functions.ns = ''
-      set result (nix-search-tv print | fzf --preview='nix-search-tv preview {}')
-      if [ "$result" ]
-          nix-search-tv preview "$result"
-      end
+      set print_command "nix-search-tv print"
+      set preview_command "nix-search-tv preview {}"
+      set result (nix-search-tv print | fzf --preview "$preview_command" \
+                                --scheme history \
+                                --header '^a all ^n nixos ^p nixpkgs ^m home-manager ^v nixvim'\
+                                --prompt "all: " \
+                                --bind "ctrl-a:change-prompt(all: )+reload($print_command)+change-preview($preview_command)" \
+                                --bind "ctrl-n:change-prompt(nixos: )+reload($print_command --indexes nixos)+change-preview($preview_command --indexes nixos)" \
+                                --bind "ctrl-p:change-prompt(nixpkgs: )+reload($print_command --indexes nixpkgs)+change-preview($preview_command --indexes nixpkgs)" \
+                                --bind "ctrl-m:change-prompt(home-manager: )+reload($print_command --indexes home-manager)+change-preview($preview_command --indexes home-manager)" \
+                                --bind "ctrl-v:change-prompt(nixvim: )+reload($print_command --indexes nixvim)+change-preview($preview_command --indexes nixvim)")
+
+        if [ "$result" ]
+            nix-search-tv preview "$result"
+        end
     '';
 
     nix-index = {
