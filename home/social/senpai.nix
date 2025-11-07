cbfc7ba174ea3c36946f3057aea219a16682ceeb
max <max@macks.cloud>
2025-11-07 21:53:03 +0100

    senpai: use latest git version for custom keybind support
    latest tagged version doesnt have it :/

--- home/social/senpai.nix
+++ home/social/senpai.nix
@@ -1,8 +1,21 @@
-{ config, ... }:
+{ config, pkgs, ... }:
+let
+  senpai-git = pkgs.senpai.overrideAttrs {
+    version = "git";
+    src = pkgs.fetchFromSourcehut {
+      owner = "~delthas";
+      repo = "senpai";
+      rev = "8a399a916e2b1277dd792df446a0dde37d0d3c17";
+      sha256 = "sha256-7vCNX5vTbZAmlpRROeeG53+L+xd4AizyQqXd2LSa1xM=";
+    };
+    vendorHash = "sha256-9IHUi7cCE/bfHAKezJYeAFPQaidWjSAYZqfEtbyV0hE=";
+  };
+in
 {
   sops.secrets.soju-password = { };
   programs.senpai = {
     enable = true;
+    package = senpai-git;
     config = {
       nickname = "mxtw";
       password-cmd = [ "cat" config.sops.secrets.soju-password.path ];
@@ -10,6 +23,13 @@
       typings = false;
       colors.nicks = "extended";
       colors.unread = "#b4befe";
+      shortcuts = {
+        "Alt+n" = "buffer-next-unread";
+        "Alt+p" = "buffer-previous-unread";
+        "Alt+d" = "scroll-next-highlight";
+        "Alt+u" = "scroll-previous-highlight";
+        "Control+r" = "search-editor";
+      };
     };
   };
 }
