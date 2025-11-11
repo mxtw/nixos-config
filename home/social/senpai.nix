e0569cb4b007d12d34ce8aa3d7238ab7b3f02058
max <max@macks.cloud>
2025-11-11 18:41:21 +0100

    senpai: use base colors for nicks
    extended can be a bit hard do read at times

--- home/social/senpai.nix
+++ home/social/senpai.nix
@@ -21,7 +21,7 @@ in
       password-cmd = [ "cat" config.sops.secrets.soju-password.path ];
       address = "ircs://irc.macks.cloud:6697";
       typings = false;
-      colors.nicks = "extended";
+      colors.nicks = "base";
       colors.unread = "#b4befe";
       shortcuts = {
         "Alt+n" = "buffer-next-unread";
