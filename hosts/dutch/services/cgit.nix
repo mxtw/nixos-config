ee06768f052e2bbb7c15f7e3e0e511b07656973e
max <max@macks.cloud>
2025-11-15 14:27:36 +0100

    git: add phone ssh key
    for android password store. sadly doesnt support ed25519

--- hosts/dutch/services/cgit.nix
+++ hosts/dutch/services/cgit.nix
@@ -27,8 +27,9 @@
       createHome = true;
       shell = "${pkgs.git}/bin/git-shell";
       openssh.authorizedKeys.keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICOZx2ejqskuEDw9ZagBJ/srl0kZHki5VwYa2oxM/+Yi"
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ96dj94d7joqGSLX4NPiFFk9rsXrw4lUlP5LnyU5ZZT"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICOZx2ejqskuEDw9ZagBJ/srl0kZHki5VwYa2oxM/+Yi max"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ96dj94d7joqGSLX4NPiFFk9rsXrw4lUlP5LnyU5ZZT max@work"
+        "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBCESfnqQwqEAFnelTHlzaMDzYZqQdYKvvp9+mvzz6pWf5SeiiGQD2sAtYr/j49NRCQ9mGqEoClk+i/hWatLMejI= max@phone"
       ];
     };
     groups.git = { };
