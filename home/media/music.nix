61e59abd3e2af7e3146953af9e546081390398fa
max <max@macks.cloud>
2025-12-28 18:01:48 +0100

    music: enable mopidy + ncmpcpp

--- home/media/music.nix
+++ home/media/music.nix
@@ -4,4 +4,24 @@
     spotify
     strawberry
   ];
+
+  services.mopidy = {
+    enable = true;
+    settings = {
+      file = {
+        media_dirs = [ "~/Music|Music" ];
+      };
+      local = {
+        media_dir = "~/Music";
+      };
+    };
+    extensionPackages = with pkgs; [
+      mopidy-mpd
+      mopidy-local
+    ];
+  };
+  programs.ncmpcpp = {
+    enable = true;
+    mpdMusicDir = "~/Music";
+  };
 }
