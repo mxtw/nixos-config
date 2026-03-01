6dc9d03dfd120bf01cad7bcbbd99f700484a93f7
max <max@macks.cloud>
2026-03-01 14:12:55 +0100

    scripts: extract scripts into standalone scripts

--- modules/home/scripts.nix
+++ modules/home/scripts.nix
@@ -1,51 +1,18 @@
 {
   flake.modules.homeManager.scripts = { pkgs, ... }:
     let
-      powermenu = pkgs.writeShellScriptBin "powermenu" ''
-        CHOICE=$(cat <<EOF | fuzzel --dmenu
-        suspend
-        poweroff
-        reboot
-        quit
-        EOF
-        )
-
-        case $CHOICE in
-            suspend)
-                systemctl suspend
-                ;;
-            poweroff)
-                systemctl poweroff
-                ;;
-            reboot)
-                systemctl reboot
-                ;;
-            quit)
-                niri msg action quit
-                ;;
-        esac
-      '';
-
-      ocr = pkgs.writeShellScriptBin "ocr" ''
-        #!/usr/bin/env bash
-        grimshot save area - | tesseract stdin - | wl-copy
-      '';
-
-      pass-fuzzel = pkgs.writeShellScriptBin "pass-fuzzel" ''
-        selection=$(find "$HOME"/.password-store -type f -name '*.gpg' -printf '%P\n' | sed 's/.gpg$//g' | sort | fuzzel --width 60 --dmenu)
-
-        if [ -n "$selection" ]; then
-            pass show -c "$selection"
-        fi
-      '';
+      powermenu = pkgs.writeShellScriptBin "powermenu" ./scripts/powermenu;
+      ocr = pkgs.writeShellScriptBin "ocr" ./scripts/ocr;
+      pass-fuzzel = pkgs.writeShellScriptBin "pass-fuzzel" ./scripts/pass-fuzzel;
     in
     {
-      home.packages = with pkgs;[
-        powermenu
-        ocr
-        tesseract
-        libnotify
-        pass-fuzzel
-      ];
+      home.packages = with pkgs;
+        [
+          powermenu
+          ocr
+          tesseract
+          libnotify
+          pass-fuzzel
+        ];
     };
 }
