4ce75bba8895f2eb8832f511438654d118277c47
max <max@macks.cloud>
2026-03-01 14:13:35 +0100

    scripts: add script to upload to dutch

--- modules/home/scripts.nix
+++ modules/home/scripts.nix
@@ -4,15 +4,17 @@
       powermenu = pkgs.writeShellScriptBin "powermenu" ./scripts/powermenu;
       ocr = pkgs.writeShellScriptBin "ocr" ./scripts/ocr;
       pass-fuzzel = pkgs.writeShellScriptBin "pass-fuzzel" ./scripts/pass-fuzzel;
+      upload = pkgs.writeShellScriptBin "upload" ./scripts/upload;
     in
     {
       home.packages = with pkgs;
         [
-          powermenu
-          ocr
-          tesseract
           libnotify
+          ocr
           pass-fuzzel
+          powermenu
+          tesseract
+          upload
         ];
     };
 }
