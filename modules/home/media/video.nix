0005318b176d8019f6ee0f3e0e787356d5f469de
max <max@macks.cloud>
2026-03-15 21:00:40 +0100

    kdenlive: remove ffmpeg override

--- modules/home/media/video.nix
+++ modules/home/media/video.nix
@@ -1,42 +1,34 @@
 {
-  flake.modules.homeManager.video = { pkgs, ... }:
-    let
-      # temporary workaround to make the build work again, see
-      # https://github.com/NixOS/nixpkgs/issues/483540#issuecomment-3839811537
-      kdenlive-ffmpeg7 = pkgs.kdePackages.kdenlive.override {
-        ffmpeg-full = pkgs.ffmpeg_7-full;
-      };
-    in
-    {
-      programs.mpv = {
-        enable = true;
-        config = {
-          vo = "gpu";
-          hwdec = "vaapi";
-          keep-open = true;
-          fullscreen = true;
-          volume = 100;
-          ytdl-format = "bestvideo+bestaudio";
-          ytdl-raw-options-append = [
-            "cookies-from-browser=chromium:~/.local/share/qutebrowser"
-            "mark-watched="
-          ];
-          demuxer-max-bytes = "1GiB";
-        };
-      };
-
-      programs.obs-studio = {
-        enable = true;
-        plugins = with pkgs.obs-studio-plugins; [
-          wlrobs
-          obs-vaapi
+  flake.modules.homeManager.video = { pkgs, ... }: {
+    programs.mpv = {
+      enable = true;
+      config = {
+        vo = "gpu";
+        hwdec = "vaapi";
+        keep-open = true;
+        fullscreen = true;
+        volume = 100;
+        ytdl-format = "bestvideo+bestaudio";
+        ytdl-raw-options-append = [
+          "cookies-from-browser=chromium:~/.local/share/qutebrowser"
+          "mark-watched="
         ];
+        demuxer-max-bytes = "1GiB";
       };
+    };
 
-      home.packages = [
-        kdenlive-ffmpeg7
-        pkgs.ffmpeg
-        pkgs.yt-dlp
+    programs.obs-studio = {
+      enable = true;
+      plugins = with pkgs.obs-studio-plugins; [
+        wlrobs
+        obs-vaapi
       ];
     };
+
+    home.packages = [
+      pkgs.kdePackages.kdenlive
+      pkgs.ffmpeg
+      pkgs.yt-dlp
+    ];
+  };
 }
