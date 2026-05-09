93576643c8a451439c2edb77fab4e58aeb872c5f
max <max@macks.cloud>
2026-05-09 12:36:21 +0200

    copyparty: local upload script for using u2c.py

--- modules/home/scripts.nix
+++ modules/home/scripts.nix
@@ -1,12 +1,20 @@
 {
-  flake.modules.homeManager.scripts = { pkgs, ... }:
+  flake.modules.homeManager.scripts = { pkgs, config, ... }:
     let
       powermenu = pkgs.writeShellScriptBin "powermenu" ./scripts/powermenu;
       ocr = pkgs.writeShellScriptBin "ocr" ./scripts/ocr;
       pass-fuzzel = pkgs.writeShellScriptBin "pass-fuzzel" ./scripts/pass-fuzzel;
-      upload = pkgs.writeShellScriptBin "upload" ./scripts/upload;
+      upload = pkgs.writeShellScriptBin "upload" ''
+        #!/usr/bin/env bash
+        ${pkgs.copyparty-min}/bin/u2c \
+            -a $(cat ${config.sops.secrets.copyparty-credentials.path}) \
+            -u \
+            "https://files.macks.cloud/''${2:-ss}" \
+        $1
+      '';
     in
     {
+      sops.secrets.copyparty-credentials = { };
       home.packages = with pkgs;
         [
           libnotify
@@ -18,3 +26,4 @@
         ];
     };
 }
+
